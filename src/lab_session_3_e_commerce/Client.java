0eb0469d883d90256b223d6c26908b3a473ab9c7
devhaan <devemahore@gmail.com>
2024-12-23 21:44:29 +0530

    code tested

--- src/lab_session_3_e_commerce/Client.java
+++ src/lab_session_3_e_commerce/Client.java
@@ -4,39 +4,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class Client implements Comparable<Client>{
-
-    int test;
-
-    public Client(int test) {
-        this.test = test;
-    }
-
-    @Override
-    public int compareTo(Client c) {
-        if(c.test == this.test){
-            return 0;
-        }
-        if (c.test > this.test) {
-            return 1;
-        }
-        return -1;
-    }
+public class Client {
 
     public static void main(String[] args) {
-        ArrayList<Client> test = new ArrayList<>();
-        Client c0 = new Client(3);
-        Client c1 = new Client(2);
-        Client c2 = new Client(1);
+        ArrayList<Item> items = new ArrayList<Item>();
 
-        test.add(c0);
-        test.add(c1);
-        test.add(c2);
-
-        Collections.sort(test, new Client());
+        items.add(new Electronic("1","iron", 1200, 1,2));
+        items.add(new Book("2","desh", 1200, 1,"devendar mahor" ));
+        items.add(new Clothing("3","t-shirt", 1200, 1, 20));
 
 
-
+        for (Item item : items) {
+            System.out.println(item.getName());
+        }
+        Collections.sort(items);
+        for (Item item : items) {
+            System.out.println(item.getName());
+        }
 
 
     }
